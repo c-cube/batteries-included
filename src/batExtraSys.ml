35fd3f8911c7b014c1b079d6e282880ccbf3bf20
Simon Cruanes <simon.cruanes.2007@m4x.org>
2014-12-20 12:04:35 +0100

    wip: split BatSys

---
+++ src/batExtraSys.ml
@@ -0,0 +1,5 @@
+
+open BatSys
+
+let files_of d = BatExtraArray.enum (readdir d)
+
---
+++ src/batExtraSys.mli
@@ -0,0 +1,4 @@
+
+val files_of: string -> string BatEnum.t
+    (**As {!readdir} but the results are presented as an enumeration
+       of names.*)
